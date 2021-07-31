9563fee6ac741a789b760dc4978fb9fbe93b1b63
grafox <grafox2021@gmail.com>
2021-07-31 21:10:42 +0200

    cambios 31 de julio

---
+++ js/vermas.js
@@ -0,0 +1,12 @@
+function vermas(id){
+    if(id=="mas"){
+    document.getElementById("desplegar").style.display="block";   
+    document.getElementById("mas").style.display="none";    
+    document.getElementById("chao").style.display="none";
+    }
+    else{
+    document.getElementById("desplegar").style.display="none";
+    document.getElementById("mas").style.display="inline";
+    document.getElementById("chao").style.display="inline";
+    }
+    }
